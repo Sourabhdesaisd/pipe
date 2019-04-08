a0435c01b43dc2d04b487d96443857833d6e1efc
Sourabhdesaisd <sourabhdesai1818@gmail.com>
2019-04-08 07:48:18 +0530

    Initial commit

--- decode/register_file.v
+++ decode/register_file.v
@@ -8,24 +8,24 @@ module register_file (
     input [4:0] rs1_addr,
     input [4:0] rs2_addr,
 
-    output wire [31:0] op1,
-    output wire [31:0] op2
+    output wire [31:0] rs1,
+    output wire [31:0] rs2
 );
     reg [31:0] reg_file [0:31];
 
-    wire [31:0] op1_forwarded;
-    wire [31:0] op2_forwarded;
+    wire [31:0] rs1_forwarded;
+    wire [31:0] rs2_forwarded;
     initial begin
         $readmemh("reg_mem.hex", reg_file);
     end
 
     // Operand forwarding if read and write addr are same
-    assign op1_forwarded = ((rs1_addr == wr_addr) && wr_en) ? wr_data : reg_file[rs1_addr];
-    assign op2_forwarded = ((rs2_addr == wr_addr) && wr_en) ? wr_data : reg_file[rs2_addr];
+    assign rs1_forwarded = ((rs1_addr == wr_addr) && wr_en) ? wr_data : reg_file[rs1_addr];
+    assign rs2_forwarded = ((rs2_addr == wr_addr) && wr_en) ? wr_data : reg_file[rs2_addr];
 
     // x0 is hardwired to 0
-    assign op1 = (rs1_addr == 0) ? 32'b0 : op1_forwarded;
-    assign op2 = (rs2_addr == 0) ? 32'b0 : op2_forwarded;
+    assign rs1 = (rs1_addr == 0) ? 32'b0 : rs1_forwarded;
+    assign rs2 = (rs2_addr == 0) ? 32'b0 : rs2_forwarded;
 
     // Write operation
     always @(posedge clk) begin
